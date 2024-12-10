e21366fa660559eee7e56f482b190ef9e645a91c
DavideFattori <fattorid5@gmail.com>
2024-12-10 16:08:52 +0100

    implementato l'infinite scroll

--- src/pages/AppHome.jsx
+++ src/pages/AppHome.jsx
@@ -1,4 +1,6 @@
-import useFetch from "../hooks/useFetch";
+import { useState, useEffect } from "react";
+import { useAsyncList } from "react-stately";
+import { useInView } from "react-intersection-observer";
 import Loader from "../components/Loader";
 import Card from "../components/Card";
 import Header from "../components/Header";
@@ -6,7 +8,47 @@ import Sidebar from "../components/Sidebar";
 
 export default function AppHome() {
 
-    const { data: games, loading: loadingGames, error: errorGames } = useFetch(`${import.meta.env.VITE_API_BASE_URL}games?key=${import.meta.env.VITE_API_KEY}&dates=2023-01-01,2024-01-12`);
+    // const [focus, setFocus] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    let games = useAsyncList({
+        async load({ signal, cursor }) {
+            setLoading(true);
+           // try {
+                let response = await fetch(cursor || `${import.meta.env.VITE_API_BASE_URL}games?key=${import.meta.env.VITE_API_KEY}&dates=2023-01-01,2024-01-12&page=1`, { signal });
+                let json = await response.json();
+                setLoading(false);
+                return {items: json.results, cursor: json.next};
+            // } catch (error) {
+            //     setLoading(false);
+            //     setError(`Error: ${error.message}`);
+            // }
+        }
+    });
+
+    const {ref, inView} = useInView({
+        threshold: 1
+    });
+
+    useEffect(() => {
+        if (inView && !games.isLoading && games.items.length) {
+            games.loadMore();
+        }
+    }, [inView, games]);
+
+
+    console.log(games);
+    
+
+
+    // const handleFocus = () => {
+    //     setFocus(true);
+    // }
+
+    // const handleBlur = () => {
+    //     setFocus(false);
+    // }
 
     return (
         <div>
@@ -15,21 +57,21 @@ export default function AppHome() {
                     <div className="col-12 p-0 mb-3">
                         <Header />
                     </div>
-                    {loadingGames ? <div className="w-100 d-flex justify-content-center"><Loader /></div> : null}
-                    {errorGames ? <div className="w-100 d-flex justify-content-center">{errorGames}</div> : null}
+                    {loading ? <div className="w-100 d-flex justify-content-center"><Loader /></div> : null}
+                    {error ? <div className="w-100 d-flex justify-content-center">{error}</div> : null}
                     <div className="col-2">
                         <Sidebar />
                     </div>
                     <div className="col-10">
                         <div className="row">
-                            {games && games.map(game => (
+                            {games.items && games.items.map(game => (
                                 <div key={game.id} className="col-6 col-md-4 p-0">
                                     <Card game={game} />
                                 </div>
                             ))}
                         </div>
+                        <div className="w-100 d-flex justify-content-center" ref={ref}><Loader /></div>
                     </div>
-                    
                 </div>
             </div>
         </div>
