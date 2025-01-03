fce914559d2af1455c48c95c4f17f2b545fdc95d
DavideFattori <fattorid5@gmail.com>
2025-01-03 15:17:14 +0100

    prova messa online 2

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 export default function AppLayout() {
     return (
         <div>
-            <Navbar />
+            {/* <Navbar /> */}
             <Outlet />
         </div>
     );
